a20a0397da94921f12d397c56b22573147798d7e
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2016-02-20 15:11:18 -0500

    Refactor: Moved QR code function

--- app/src/main/java/com/mchacks/qrtransfer/SendFileActivity.java
+++ app/src/main/java/com/mchacks/qrtransfer/SendFileActivity.java
@@ -2,21 +2,17 @@ package com.mchacks.qrtransfer;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.mchacks.qrtransfer.processing.QRProcessor;
 import com.nononsenseapps.filepicker.FilePickerActivity;
 import android.widget.ImageView;
 
-import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 
 
@@ -35,7 +31,6 @@ public class SendFileActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 // This always works
                 Intent i = new Intent(view.getContext(), FilePickerActivity.class);
                 // This works if you defined the intent filter
@@ -62,7 +57,7 @@ public class SendFileActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         try{
-            Bitmap bmp = generateQrCode("Hello Andrew!");
+            Bitmap bmp = QRProcessor.generateQrCode("Hello Andrew!");
             ImageView iv = (ImageView) findViewById(R.id.image);
             iv.setImageBitmap(bmp);
         } catch (WriterException e)
@@ -70,23 +65,6 @@ public class SendFileActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-    }
-    public static Bitmap generateQrCode(String myCodeText) throws WriterException
-    {
-
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-
-        int size = 256;
-
-        BitMatrix bitMatrix = qrCodeWriter.encode(myCodeText,BarcodeFormat.QR_CODE, size, size);
-        int width = bitMatrix.getWidth();
-        Bitmap bmp = Bitmap.createBitmap(width, width, Bitmap.Config.RGB_565);
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < width; y++) {
-                bmp.setPixel(y, x, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
-            }
-        }
-        return bmp;
     }
 
 }
--- app/src/main/java/com/mchacks/qrtransfer/processing/QRProcessor.java
+++ app/src/main/java/com/mchacks/qrtransfer/processing/QRProcessor.java
@@ -1,12 +1,37 @@
 package com.mchacks.qrtransfer.processing;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.media.Image;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 import java.io.File;
 import java.util.LinkedList;
 
 
 public class QRProcessor implements QRInterface {
+
+    public static Bitmap generateQrCode(String myCodeText) throws WriterException
+    {
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+        int size = 256;
+
+        BitMatrix bitMatrix = qrCodeWriter.encode(myCodeText, BarcodeFormat.QR_CODE, size, size);
+        int width = bitMatrix.getWidth();
+        Bitmap bmp = Bitmap.createBitmap(width, width, Bitmap.Config.RGB_565);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < width; y++) {
+                bmp.setPixel(y, x, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+            }
+        }
+        return bmp;
+    }
 
     /**
      * Encode a file as QR code images.

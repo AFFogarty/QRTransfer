fa43638a24ae55f4949588db11392c5c88d516e0
Alex Parmentier <a.g.parmentier@gmail.com>
2016-02-20 15:05:54 -0500

    WIP: Function for generating QR code.
    It takes a string and generates a QR code bitmap that can not yet be displayed.

--- app/src/main/java/com/mchacks/qrtransfer/SendFileActivity.java
+++ app/src/main/java/com/mchacks/qrtransfer/SendFileActivity.java
@@ -1,11 +1,22 @@
 package com.mchacks.qrtransfer;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import android.widget.ImageView;
+
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+
+
 
 public class SendFileActivity extends AppCompatActivity {
 
@@ -25,6 +36,33 @@ public class SendFileActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        try{
+            Bitmap bmp = generateQrCode("Hello Andrew!");
+            ImageView iv = (ImageView) findViewById(R.id.image);
+            iv.setImageBitmap(bmp);
+        } catch (WriterException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+    public static Bitmap generateQrCode(String myCodeText) throws WriterException
+    {
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+        int size = 256;
+
+        BitMatrix bitMatrix = qrCodeWriter.encode(myCodeText,BarcodeFormat.QR_CODE, size, size);
+        int width = bitMatrix.getWidth();
+        Bitmap bmp = Bitmap.createBitmap(width, width, Bitmap.Config.RGB_565);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < width; y++) {
+                bmp.setPixel(y, x, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+            }
+        }
+        return bmp;
     }
 
 }

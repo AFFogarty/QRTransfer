a72bc7890622dce01f3357ac942353902db4378e
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2016-02-20 17:45:18 -0500

    Fixed: Constants public

--- app/src/main/java/com/mchacks/qrtransfer/util/Constants.java
+++ app/src/main/java/com/mchacks/qrtransfer/util/Constants.java
@@ -1,6 +1,6 @@
 package com.mchacks.qrtransfer.util;
 
 public class Constants {
-    static final int qrWidth = 800;
-    static final int qrHeight = 800;
+    public static final int qrWidth = 800;
+    public static final int qrHeight = 800;
 }

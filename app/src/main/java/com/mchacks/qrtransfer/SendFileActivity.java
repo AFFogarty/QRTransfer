41c1f4e91f608ec435beccbc349cfdc6a8a2c103
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2016-02-20 15:06:19 -0500

    New: File chooser

--- app/src/main/java/com/mchacks/qrtransfer/SendFileActivity.java
+++ app/src/main/java/com/mchacks/qrtransfer/SendFileActivity.java
@@ -1,13 +1,19 @@
 package com.mchacks.qrtransfer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.nononsenseapps.filepicker.FilePickerActivity;
+
 public class SendFileActivity extends AppCompatActivity {
+
+    private static final int FILE_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +26,28 @@ public class SendFileActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                // This always works
+                Intent i = new Intent(view.getContext(), FilePickerActivity.class);
+                // This works if you defined the intent filter
+                // Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+
+                // Set these depending on your use case. These are the defaults.
+                i.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false);
+                i.putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, false);
+                i.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_FILE);
+
+                // Configure initial directory by specifying a String.
+                // You could specify a String like "/storage/emulated/0/", but that can
+                // dangerous. Always use Android's API calls to get paths to the SD-card or
+                // internal memory.
+                i.putExtra(FilePickerActivity.EXTRA_START_PATH, Environment.getExternalStorageDirectory().getPath());
+
+                startActivityForResult(i, FILE_CODE);
+
+
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);

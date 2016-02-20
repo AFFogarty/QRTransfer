5ea84b99f4ecd1417eff39e8ec5098398a78f51f
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2016-02-20 17:58:57 -0500

    New: Show how many qr codes are displaying

--- app/src/main/java/com/mchacks/qrtransfer/PlayImageSlideshowActivity.java
+++ app/src/main/java/com/mchacks/qrtransfer/PlayImageSlideshowActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.zxing.WriterException;
 import com.mchacks.qrtransfer.processing.BitmapProcessor;
@@ -51,16 +52,22 @@ public class PlayImageSlideshowActivity extends AppCompatActivity {
         // Final red image at end
         qrCodes.add(BitmapProcessor.createImage(Constants.qrDimension, Constants.qrDimension, Color.RED));
 
-        final ImageView iv = (ImageView) findViewById(R.id.imageView);
+        final ImageView qrCodeImageView = (ImageView) findViewById(R.id.imageView);
+        final TextView statusText = (TextView) findViewById(R.id.statusText);
 
         Handler[] handlers = new Handler[qrCodes.size()];
 
+        final int totalImages = qrCodes.size();
         for (int i = 0; i < qrCodes.size(); i++) {
             handlers[i] = new Handler();
             final Bitmap qrCode = qrCodes.get(i);
+            final int current = i + 1;
             handlers[i].postDelayed(new Runnable() {
                 public void run() {
-                    iv.setImageBitmap(qrCode);
+                    // Set the status text
+                    statusText.setText(String.format("%d/%d", current, totalImages));
+                    // Set the
+                    qrCodeImageView.setImageBitmap(qrCode);
                 }
             }, i *3000);
         }

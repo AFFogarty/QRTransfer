3288ac9f91b03e6c2925129a6ed0ac9eb9ee94e2
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2016-02-20 21:58:29 -0500

    optimized imports

--- app/src/main/java/com/mchacks/qrtransfer/MainActivity.java
+++ app/src/main/java/com/mchacks/qrtransfer/MainActivity.java
@@ -2,11 +2,8 @@ package com.mchacks.qrtransfer;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {

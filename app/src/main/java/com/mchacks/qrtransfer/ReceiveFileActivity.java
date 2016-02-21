b8914b4a26da0ae4943d0282b99f5c417b6936f3
Alex Parmentier <a.g.parmentier@gmail.com>
2016-02-21 11:13:40 -0500

    More camera stuff and signing.

--- app/src/main/java/com/mchacks/qrtransfer/ReceiveFileActivity.java
+++ app/src/main/java/com/mchacks/qrtransfer/ReceiveFileActivity.java
@@ -1,6 +1,9 @@
 package com.mchacks.qrtransfer;
 
 
+import android.content.Intent;
+import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,14 +12,19 @@ import android.view.KeyEvent;
 import android.view.View;
 
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.zxing.ResultPoint;
 
 import com.journeyapps.barcodescanner.BarcodeCallback;
 import com.journeyapps.barcodescanner.BarcodeResult;
 import com.journeyapps.barcodescanner.CompoundBarcodeView;
+import com.mchacks.qrtransfer.processing.QRProcessor;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 
 public class ReceiveFileActivity extends AppCompatActivity {
@@ -33,8 +41,7 @@ public class ReceiveFileActivity extends AppCompatActivity {
         public void barcodeResult(BarcodeResult result) {
             String resText = result.getText();
             if (resText != null) {
-                if (!resText.equals(lastScanned))
-                {
+                if (!resText.equals(lastScanned)) {
                     scannedCount++;
                     barcodeView.setStatusText("Scanned " + scannedCount + " code(s).");
                     scannedCodes.add(result);
@@ -48,15 +55,43 @@ public class ReceiveFileActivity extends AppCompatActivity {
         public void possibleResultPoints(List<ResultPoint> resultPoints) {
         }
     };
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_file);
-
+        Camera mcamera = Camera.open();
         barcodeView = (CompoundBarcodeView) findViewById(R.id.barcode_scanner);
         barcodeView.decodeContinuous(callback);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    public void onClickBackButton(View view)
+    {
+        Intent intent = new Intent(view.getContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickFinishButton(View view)
+    {
+        barcodeView.pause();
+        StringBuilder strbuilder = new StringBuilder();
+        for (BarcodeResult code : scannedCodes) {
+            strbuilder.append(code.getText());
+        }
+        String result = strbuilder.toString();
+        byte[] barr = QRProcessor.parse_string(result);
+        System.out.println(result);
+    }
+
 
     @Override
     protected void onResume() {
@@ -87,6 +122,46 @@ public class ReceiveFileActivity extends AppCompatActivity {
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         return barcodeView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "ReceiveFile Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.mchacks.qrtransfer/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "ReceiveFile Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.mchacks.qrtransfer/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
 
--- app/src/main/java/com/mchacks/qrtransfer/util/Constants.java
+++ app/src/main/java/com/mchacks/qrtransfer/util/Constants.java
@@ -2,5 +2,5 @@ package com.mchacks.qrtransfer.util;
 
 public class Constants {
     public static final int qrDimension = 1000;
-    public static final int byteDensity = 1000;
+    public static final int byteDensity = 500;
 }
